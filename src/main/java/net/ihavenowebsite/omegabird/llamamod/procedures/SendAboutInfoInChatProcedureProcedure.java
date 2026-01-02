ee40fc53a6881bfbd21a9297bb353e56fdbd0d8f
Omegabird113 <omegabird113@outlook.com>
2026-01-02 15:07:03 -0500

    Updated version info to 1.0.0beta2

--- src/main/java/net/ihavenowebsite/omegabird/llamamod/procedures/SendAboutInfoInChatProcedureProcedure.java
+++ src/main/java/net/ihavenowebsite/omegabird/llamamod/procedures/SendAboutInfoInChatProcedureProcedure.java
@@ -10,6 +10,6 @@ public class SendAboutInfoInChatProcedureProcedure {
 			return;
 		if (entity instanceof Player _player && !_player.level().isClientSide())
 			_player.displayClientMessage(
-					Component.literal(("About LLamaMod -- version " + "0.46dev, " + "made for Minecraft 1.21.8 w/ Neoforge " + "-- LlamaMod is developed by Omegabird113 w/ MCreator version " + "2025.3, plus the Blockstates plugin.")), false);
+					Component.literal(("About LLamaMod -- version " + "1.0.0beta2" + "made for Minecraft 1.21.8 w/ Neoforge " + "-- LlamaMod is developed by Omegabird113 w/ MCreator version " + "2025.3, plus the Blockstates plugin.")), false);
 	}
 }
