6d5f0dba21858208fa4f457d384fd61017c10f7a
Omegabird113 <omegabird113@outlook.com>
2026-01-24 16:13:33 -0500

    Increased default Player Management Permission level from 2 to 3

--- src/main/java/mc/omegabird/llamablocks/configuration/LlamamodserverconfigConfiguration.java
+++ src/main/java/mc/omegabird/llamablocks/configuration/LlamamodserverconfigConfiguration.java
@@ -22,7 +22,7 @@ public class LlamamodserverconfigConfiguration {
 		ACCESS_EXISTING_PASSWORDS_PERMISSION_LEVEL = BUILDER.comment("This controls the permission level required for people to be able to see the existing password in the Password Changer GUI").define("password_viewing_permission_level",
 				(double) 3);
 		COMPUTER_PLAYER_MANAGEMENT_PERMISSION_LEVEL = BUILDER.comment("This controls the permission level required for people to be able to kill or clear the inventory of players in the Computer GUI").define("player_management_permission_level",
-				(double) 2);
+				(double) 3);
 		BUILDER.pop();
 		BUILDER.push("misc");
 		GIVE_ALL_RECIPES_ON_PLAYER_JOIN = BUILDER.comment("When enabled, this runs /recipe give @a * when an player joins the server or world").define("give_players_all_recipes", true);
