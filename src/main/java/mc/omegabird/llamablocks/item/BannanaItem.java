14a83ab133e61c9b36d73f8309414cb781011b07
Omegabird113 <omegabird113@outlook.com>
2026-01-24 19:33:39 -0500

    Increased Banana Plant saturation value

--- src/main/java/mc/omegabird/llamablocks/item/BannanaItem.java
+++ src/main/java/mc/omegabird/llamablocks/item/BannanaItem.java
@@ -6,6 +6,6 @@ import net.minecraft.world.food.FoodProperties;
 
 public class BannanaItem extends Item {
 	public BannanaItem(Item.Properties properties) {
-		super(properties.food((new FoodProperties.Builder()).nutrition(2).saturationModifier(3.55f).build(), Consumables.defaultFood().consumeSeconds(0.6F).build()));
+		super(properties.food((new FoodProperties.Builder()).nutrition(2).saturationModifier(4.1f).build(), Consumables.defaultFood().consumeSeconds(0.6F).build()));
 	}
 }
