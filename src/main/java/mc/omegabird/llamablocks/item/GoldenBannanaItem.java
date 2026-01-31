5ca0ca73a1168781f13967e5a1a62d402cab4a68
Omegabird113 <omegabird113@outlook.com>
2026-01-31 14:22:19 -0500

    Golden Banana rarity value is now Common (so label is white)

--- src/main/java/mc/omegabird/llamablocks/item/GoldenBannanaItem.java
+++ src/main/java/mc/omegabird/llamablocks/item/GoldenBannanaItem.java
@@ -1,12 +1,11 @@
 package mc.omegabird.llamablocks.item;
 
 import net.minecraft.world.item.component.Consumables;
-import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.food.FoodProperties;
 
 public class GoldenBannanaItem extends Item {
 	public GoldenBannanaItem(Item.Properties properties) {
-		super(properties.rarity(Rarity.UNCOMMON).food((new FoodProperties.Builder()).nutrition(5).saturationModifier(12.2f).build(), Consumables.defaultFood().consumeSeconds(0.95F).build()));
+		super(properties.food((new FoodProperties.Builder()).nutrition(5).saturationModifier(12.2f).build(), Consumables.defaultFood().consumeSeconds(0.95F).build()));
 	}
 }
