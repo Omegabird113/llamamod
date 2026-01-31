c3c2878c19e35c9a0dd02be7a717af2c3edf94f4
Omegabird113 <omegabird113@outlook.com>
2026-01-31 14:23:32 -0500

    The Golden Banana takes 1 second to eat now

--- src/main/java/mc/omegabird/llamablocks/item/GoldenBannanaItem.java
+++ src/main/java/mc/omegabird/llamablocks/item/GoldenBannanaItem.java
@@ -6,6 +6,6 @@ import net.minecraft.world.food.FoodProperties;
 
 public class GoldenBannanaItem extends Item {
 	public GoldenBannanaItem(Item.Properties properties) {
-		super(properties.food((new FoodProperties.Builder()).nutrition(5).saturationModifier(12.2f).build(), Consumables.defaultFood().consumeSeconds(0.95F).build()));
+		super(properties.food((new FoodProperties.Builder()).nutrition(5).saturationModifier(12.2f).build(), Consumables.defaultFood().consumeSeconds(1F).build()));
 	}
 }
