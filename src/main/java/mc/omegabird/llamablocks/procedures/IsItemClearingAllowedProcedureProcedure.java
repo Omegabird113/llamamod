b9c926d9579dd67c7c4e41253a525745f0867374
Omegabird113 <omegabird113@outlook.com>
2026-02-10 15:39:33 -0500

    Removed old procedure which stopped loading

--- src/main/java/mc/omegabird/llamablocks/procedures/IsItemClearingAllowedProcedureProcedure.java
+++
@@ -1,9 +0,0 @@
-package mc.omegabird.llamablocks.procedures;
-
-import mc.omegabird.llamablocks.configuration.LlamamodserverconfigConfiguration;
-
-public class IsItemClearingAllowedProcedureProcedure {
-	public static boolean execute() {
-		return LlamamodserverconfigConfiguration.ALLOW_ITEM_CLEARING.get();
-	}
-}
