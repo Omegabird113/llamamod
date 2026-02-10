7bef15759b060587298e2ddef3f1d81af5417f22
Omegabird113 <omegabird113@outlook.com>
2026-02-10 14:31:58 -0500

    Gold Banana Effects Buff

--- src/main/java/mc/omegabird/llamablocks/procedures/GoldenBannanaPlayerFinishesUsingItemProcedure.java
+++ src/main/java/mc/omegabird/llamablocks/procedures/GoldenBannanaPlayerFinishesUsingItemProcedure.java
@@ -10,12 +10,12 @@ public class GoldenBannanaPlayerFinishesUsingItemProcedure {
 		if (entity == null)
 			return;
 		if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-			_entity.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 14, 0));
+			_entity.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 16, 0));
 		if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-			_entity.addEffect(new MobEffectInstance(MobEffects.SATURATION, 80, 0));
+			_entity.addEffect(new MobEffectInstance(MobEffects.SATURATION, 200, 0));
 		if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
 			_entity.addEffect(new MobEffectInstance(MobEffects.LUCK, 900, 0));
 		if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-			_entity.addEffect(new MobEffectInstance(MobEffects.HASTE, 600, 0));
+			_entity.addEffect(new MobEffectInstance(MobEffects.HASTE, 800, 0));
 	}
 }
