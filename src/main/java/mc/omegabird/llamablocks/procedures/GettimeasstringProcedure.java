24f1e1599939caa5e53cb80259878e65fe299a31
Omegabird113 <omegabird113@outlook.com>
2026-02-10 15:00:52 -0500

    Attem[t to add 12hr time

--- src/main/java/mc/omegabird/llamablocks/procedures/GettimeasstringProcedure.java
+++ src/main/java/mc/omegabird/llamablocks/procedures/GettimeasstringProcedure.java
@@ -9,12 +9,27 @@ public class GettimeasstringProcedure {
 		double gametime = 0;
 		double daytime = 0;
 		double minute = 0;
+		double twelve_hour_hour = 0;
+		String am_or_pm = "";
 		gametime = world.dayTime();
 		daytime = gametime % 24000;
 		twenty_four_hour_hour = (gametime / 1000 + 6) % 24;
 		minute = (twenty_four_hour_hour - Math.floor(twenty_four_hour_hour)) * 60;
 		percentage_througth_day_night = (((daytime / 24000) * 100) % 50) * 2;
-		return new java.text.DecimalFormat("#0").format(Math.floor(twenty_four_hour_hour)) + ":" + new java.text.DecimalFormat("00").format(Math.floor(minute)) + " (" + new java.text.DecimalFormat("#0.0").format(percentage_througth_day_night)
+		if (Math.floor(twenty_four_hour_hour) == 0) {
+			twelve_hour_hour = 12;
+			am_or_pm = "AM";
+		} else if (Math.floor(twenty_four_hour_hour) < 12) {
+			twelve_hour_hour = Math.floor(twenty_four_hour_hour);
+			am_or_pm = "AM";
+		} else if (Math.floor(twenty_four_hour_hour) == 12) {
+			twelve_hour_hour = 12;
+			am_or_pm = "PM";
+		} else {
+			twelve_hour_hour = Math.floor(twenty_four_hour_hour) - 12;
+			am_or_pm = "PM";
+		}
+		return new java.text.DecimalFormat("#0").format(twelve_hour_hour) + ":" + new java.text.DecimalFormat("00").format(Math.floor(minute)) + " " + am_or_pm + " (" + new java.text.DecimalFormat("#0.0").format(percentage_througth_day_night)
 				+ "% through the " + (daytime < 12000 ? "day" : "night") + ")";
 	}
 }
