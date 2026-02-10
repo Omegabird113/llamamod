0588cb73607b80d653fb7453aef3bdf4b13b16ac
Omegabird113 <omegabird113@outlook.com>
2026-02-10 14:30:35 -0500

    Made Gold Banana upgrade template texture more gold, re-ordered mod elements

--- src/main/java/mc/omegabird/llamablocks/init/LlamamodModTabs.java
+++ src/main/java/mc/omegabird/llamablocks/init/LlamamodModTabs.java
@@ -384,11 +384,11 @@ public class LlamamodModTabs {
 				tabData.accept(LlamamodModItems.PASSWORD_CHANGER.get());
 				tabData.accept(LlamamodModItems.BACKPACK.get());
 				tabData.accept(LlamamodModItems.NETHERITE_BACKPACK.get());
-				tabData.accept(LlamamodModItems.BANANA.get());
-				tabData.accept(LlamamodModItems.GOLDEN_BANANA.get());
 				tabData.accept(LlamamodModBlocks.BANANA_PLANT.get().asItem());
 				tabData.accept(LlamamodModItems.FARM_SCRAPS.get());
-				tabData.accept(LlamamodModItems.ACID_BUCKET.get());
+				tabData.accept(LlamamodModItems.BANANA.get());
+				tabData.accept(LlamamodModItems.GOLDEN_BANANA.get());
 				tabData.accept(LlamamodModItems.GOLDEN_BANANA_UPGRADE_TEMPLATE.get());
+				tabData.accept(LlamamodModItems.ACID_BUCKET.get());
 			}).build());
 }
