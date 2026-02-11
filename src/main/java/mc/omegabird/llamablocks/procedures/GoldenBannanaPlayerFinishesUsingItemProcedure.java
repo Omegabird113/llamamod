005a35d00a39db2e987d01e33b979765097bc4ca
Omegabird113 <omegabird113@outlook.com>
2026-02-11 16:22:08 -0500

    Golden banana no longer gives saturation

--- src/main/java/mc/omegabird/llamablocks/procedures/GoldenBannanaPlayerFinishesUsingItemProcedure.java
+++ src/main/java/mc/omegabird/llamablocks/procedures/GoldenBannanaPlayerFinishesUsingItemProcedure.java
@@ -17,8 +17,6 @@ public class GoldenBannanaPlayerFinishesUsingItemProcedure {
 			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
 				_entity.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 16, 0));
 			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-				_entity.addEffect(new MobEffectInstance(MobEffects.SATURATION, 200, 0));
-			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
 				_entity.addEffect(new MobEffectInstance(MobEffects.LUCK, 900, 0));
 			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
 				_entity.addEffect(new MobEffectInstance(MobEffects.HASTE, 800, 0));
