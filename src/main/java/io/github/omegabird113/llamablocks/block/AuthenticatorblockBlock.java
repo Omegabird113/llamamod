a24027f47661245668b6f6e714e371cb1a9a070b
Omegabird113 <omegabird113@outlook.com>
2026-04-05 18:26:56 -0400

    Increase Authenticator tick rate from 10 to 20 per second

--- src/main/java/io/github/omegabird113/llamablocks/block/AuthenticatorblockBlock.java
+++ src/main/java/io/github/omegabird113/llamablocks/block/AuthenticatorblockBlock.java
@@ -76,14 +76,14 @@ public class AuthenticatorblockBlock extends Block implements EntityBlock {
 	@Override
 	public void onPlace(BlockState blockstate, Level world, BlockPos pos, BlockState oldState, boolean moving) {
 		super.onPlace(blockstate, world, pos, oldState, moving);
-		world.scheduleTick(pos, this, 10);
+		world.scheduleTick(pos, this, 20);
 	}
 
 	@Override
 	public void tick(BlockState blockstate, ServerLevel world, BlockPos pos, RandomSource random) {
 		super.tick(blockstate, world, pos, random);
 		AuthblockupdateprocedureProcedure.execute(world, pos.getX(), pos.getY(), pos.getZ());
-		world.scheduleTick(pos, this, 10);
+		world.scheduleTick(pos, this, 20);
 	}
 
 	@Override
