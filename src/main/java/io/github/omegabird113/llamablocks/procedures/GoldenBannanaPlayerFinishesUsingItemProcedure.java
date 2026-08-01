6823aaa079cd468dd30fc0e615f452edee2ba7e0
omegabird113 <omegabird113@outlook.com>
2026-08-01 10:36:11 -0400

    Make Golden Banana give Regen 3 for 50 ticks instead of Regen 1 for 20 ticks

--- src/main/java/io/github/omegabird113/llamablocks/procedures/GoldenBannanaPlayerFinishesUsingItemProcedure.java
+++ src/main/java/io/github/omegabird113/llamablocks/procedures/GoldenBannanaPlayerFinishesUsingItemProcedure.java
@@ -15,7 +15,7 @@ public class GoldenBannanaPlayerFinishesUsingItemProcedure {
 			return;
 		if (world instanceof ServerLevel _serverLevelGR0 && _serverLevelGR0.getGameRules().get(LlamamodModGameRules.GIVE_GOLDEN_BANANA_EFFECTS.get())) {
 			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-				_entity.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 20, 0));
+				_entity.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 50, 2));
 			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
 				_entity.addEffect(new MobEffectInstance(MobEffects.LUCK, 900, 1));
 			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
